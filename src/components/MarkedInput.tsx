d44a2028ca49816acf94335e00d845c751227ae9
skevprog <kevin-s93@hotmail.com>
2020-09-13 14:24:09 -0300

    feat: Add title and modify styles

--- src/components/MarkedInput.tsx
+++ src/components/MarkedInput.tsx
@@ -2,21 +2,31 @@ import React, { useState } from 'react';
 import ReactMarkdown from 'react-markdown';
 import styled from 'styled-components';
 
-const InputContainer = styled.div`
-  height: 20em;
-  min-width: 10em;
-  width: 50%;
+const Container = styled.div`
   display: flex;
   width: 100%;
-  padding: 1mm;
+  height: 100%;
+  flex-direction: column;
+  align-items: center;
+  @media screen and (min-width: 800px) {
+    flex-direction: row;
+  }
+`;
+
+const InputContainer = styled.div`
+  height: 100%;
+  display: flex;
+  width: 100%;
+  padding: 1em;
+  flex-direction: column;
 `;
 
 const ResultContainer = styled.div`
-  height: 20em;
-  width: 100%;
   height: 100%;
+  width: 100%;
   font-size: 17px;
   padding: 1em;
+  box-sizing: border-box;
 `;
 
 const TextArea = styled.textarea`
@@ -30,14 +40,9 @@ const TextArea = styled.textarea`
   box-sizing: border-box;
 `;
 
-const Container = styled.div`
-  display: flex;
-  width: 100%;
-  flex-direction: column;
-  align-items: center;
-  @media screen and (min-width: 800px) {
-    flex-direction: row;
-  }
+const Title = styled.h2`
+  margin: 0 0 1em 0;
+  text-align: center;
 `;
 
 function MarkedInput() {
@@ -47,9 +52,15 @@ function MarkedInput() {
   return (
     <Container>
       <InputContainer>
-        <TextArea value={text} onChange={handleChange} />
+        <Title>Markdown Text</Title>
+        <TextArea
+          value={text}
+          placeholder="Write your markdown here"
+          onChange={handleChange}
+        />
       </InputContainer>
       <ResultContainer>
+        <Title>Converted Text</Title>
         <ReactMarkdown source={text} />
       </ResultContainer>
     </Container>
